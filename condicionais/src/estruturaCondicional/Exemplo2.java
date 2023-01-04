dc8cecf0784abcd8512a6c2fe427aa24545f4512
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2023-01-04 16:22:54 -0300

    Exemplos de IF, IF/ELSE e SWITCH CASE

---
+++ condicionais/src/estruturaCondicional/Exemplo2.java
@@ -0,0 +1,20 @@
+package estruturaCondicional;
+
+public class Exemplo2 {
+
+	public static void main(String[] args) {
+		int n1 = 4, n2 = 3, n3 = 4;
+		
+		if(n1 < 5) {
+			System.out.println("O número 1 é menor do que 5.");
+		}
+		
+		if(n1 < n2) {
+			System.out.println("O número 1 é menor do que o número 2.");
+		}
+		
+		if(n1 == n3) {
+			System.out.println("O número 1 e o número 3 são iguais.");
+		}
+	}
+}
---
+++ condicionais/src/estruturaCondicional/Exemplo3.java
@@ -0,0 +1,15 @@
+package estruturaCondicional;
+
+public class Exemplo3 {
+
+	public static void main(String[] args) {
+		int idade = 18;
+		boolean carteiraM = true;
+		
+		if(idade >= 18 && carteiraM == true) {
+			System.out.println("Você pode dirigir.");
+		}
+
+	}
+
+}
