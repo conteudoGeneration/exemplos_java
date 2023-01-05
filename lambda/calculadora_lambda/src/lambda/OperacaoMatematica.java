b105e25bd8343fafda5e4f4edbfbf4949885f06c
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-01-05 06:08:24 -0300

    Lambda

---
+++ lambda/calculadora_lambda/src/lambda/OperacaoMatematica.java
@@ -0,0 +1,8 @@
+package lambda;
+
+@FunctionalInterface
+public interface OperacaoMatematica {
+
+	public int executar(int a, int b);
+
+}
---
+++ lambda/calculadora_lambda/src/lambda/TestaCalculadora.java
@@ -0,0 +1,35 @@
+package lambda;
+
+import java.util.Scanner;
+
+public class TestaCalculadora {
+
+	public static int calcular(OperacaoMatematica om, int num1, int num2) {
+        return om.executar(num1, num2);
+    }
+	
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		int n1, n2 = 0;
+		
+		System.out.println("****************************************************");
+		System.out.println("              Calculadora com Lambda                ");
+		System.out.println("****************************************************");
+		System.out.println("Digite o primeiro numero: ");
+		n1 = leia.nextInt();
+		System.out.println("Digite o segundo numero: ");
+		n2 = leia.nextInt();
+		
+		System.out.println("Soma = " + calcular((a,b) -> a + b, n1, n2));
+        System.out.println("Subtração = " + calcular((a,b) -> a - b, n1, n2));
+        System.out.println("Multiplicação = " + calcular((a,b) -> a * b, n1, n2));
+        System.out.println("Divisão = " + calcular((a,b) -> a / b, n1, n2));
+		
+        leia.close();
+        
+	}
+		
+
+}
