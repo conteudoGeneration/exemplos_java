1c8834fe9b4a22fa55d4da54c5610d5294659cdb
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2023-01-10 15:36:28 -0300

    Add novos exemplos de forEach

---
+++ vetor_matriz/src/vetor_Matriz/Exemplo16.java
@@ -0,0 +1,12 @@
+package vetor_Matriz;
+
+public class Exemplo16 {
+
+	public static void main(String[] args) {
+		String alunes[] = { "Felipe", "Jonas", "Julia", "Marcos" };
+
+		for(int i = 0; i < alunes.length; i++) {
+			System.out.println(alunes[i]);
+		}
+	}
+}
---
+++ vetor_matriz/src/vetor_Matriz/Exemplo16_1.java
@@ -0,0 +1,12 @@
+package vetor_Matriz;
+
+public class Exemplo16_1 {
+
+	public static void main(String[] args) {
+		String alunes[] = { "Felipe", "Jonas", "Julia", "Marcos" };
+
+		for (String alune : alunes) {
+			System.out.println(alune);
+		}
+	}
+}
