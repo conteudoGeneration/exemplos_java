14c2ce1add7698dd108ac7814f9d9d845affb616
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-01-12 17:58:50 -0300

    Entrada e Saída de dados

---
+++ entrada_saida/01_helloworld/src/helloworld/Hello.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("Olá Mundo!");
+
+	}
+
+}
---
+++ entrada_saida/06_helloworld_comentarios/src/helloworld/Hello.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/** 
+ * Área reservada para importar os
+ * Pacotes.
+ * 
+ * */
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		// Exibe a Mensagem na tela
+		System.out.print("Olá Mundo!");
+
+	}
+
+}
